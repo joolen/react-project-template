45e12d50c06316769a361ee5aa1d60ae03a2d444
Yosuke Enomoto <yosukeenomoto@YosukenoMacBook-Pro.local>
2019-08-27 14:09:15 +0900

    add CardItem component

--- src/components/stories/organisms.stories.tsx
+++ src/components/stories/organisms.stories.tsx
@@ -3,10 +3,19 @@ import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 import { withKnobs, text } from "@storybook/addon-knobs";
 import { SampleAppBar } from "../organisms/AppBar";
+import { CardItem } from "../organisms/CardItem";
 
 export const atoms_effects = storiesOf("Organisms", module)
-    .addDecorator(withInfo({ inline: true }))
+    .addDecorator(withInfo())
     .addDecorator(withKnobs)
-    .add("PrimaryButton", () => (
+    .add("Appbar", () => (
         <SampleAppBar title={text("title", "sample")} />
+    ))
+    .add("CardItem", () => (
+        <CardItem
+            image={text("image", "https://redux.js.org/img/redux.svg")}
+            title={text("title", "Redux")}
+            describe={text("describe", "A predictable state container for JavaScript apps.")}
+            learnMoreUrl={text("learn more url", "https://redux.js.org/")}
+        />
     ))
